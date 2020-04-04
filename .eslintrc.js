eeef081dab80c094a92c2902d061fad084034487
Brandon Martel <brandon@pepper.com>
2020-04-04 12:01:27 -0500

    adding prettier dependency and plugin def in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: ['prettier'],
-  plugins: ['import', 'react'],
+  plugins: ['prettier', 'import', 'react'],
   rules: {
     'no-unused-vars': [2, { varsIgnorePattern: 'h' }],
     'react/jsx-uses-vars': 2,
     'no-undef': 0,
+    'prettier/prettier': 'error',
   }
 }
